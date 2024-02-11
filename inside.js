c2eee49bda0b8b8bae0e11e3326b67bcd09efae1
Daniel Zeh <dfzeh1@gmail.com>
2024-02-10 23:25:20 -0600

    made more changes

---
+++ inside.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const Database = ["shirt", "shoes", "pants", "wallet"]
+
+app.get('http://localhost:5500/api/inventory', (req,res)=> {
+    console.log(Database)
+    res.status(200).send(Database)
+})
+
+
+const SERVER_PORT = '5500';
+app.listen(SERVER_PORT,() => console.log(`up and running on ${SERVER_PORT}`)) 
