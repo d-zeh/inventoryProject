e5b944e9c728c25a5aa6b8a139584c3257e87033
Daniel Zeh <dfzeh1@gmail.com>
2024-02-15 23:25:28 -0600

    a lot of clean up

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
 const Database = ["shirt", "shoes", "pants", "wallet"]
 
-app.get('http://localhost:5500/api/inventory', (req,res)=> {
-    console.log(Database)
-    res.status(200).send(Database)
-})
+// app.get('http://localhost:5500/api/inventory', (req,res)=> {
+//     console.log(Database)
+//     res.status(200).send(Database)
+// })
+console.log("hi")
 
 
 //require not running 
