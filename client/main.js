890aaa4cca9b62da5c7a8aecb190e90cee6535d0
Daniel Zeh <dfzeh1@gmail.com>
2024-02-17 16:18:58 -0600

    few changes to page

--- client/main.js
+++ client/main.js
@@ -2,6 +2,10 @@ const mainBox = document.querySelector('.main')
 console.log("ho")
 function sampleText() {
      const card = document.createElement('div')
+
+     //WHen a certain thing is sent from the home page
+     //Get request populate the from the database
+     //Select, delete, edit
      card.innerHTML = `
      <div> Words 
      YAYYYYYYYY</div>
