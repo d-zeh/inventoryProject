e21ea45c33d1e08a58e8594e0810038a496832a7
Daniel Zeh <dfzeh1@gmail.com>
2024-02-20 12:04:42 -0600

    get All buttom

--- client/main.js
+++ client/main.js
@@ -1,19 +1,56 @@
 const mainBox = document.querySelector('.main')
+let database = {
+     name: "Protein",
+     proteinAmount: 100,
+     price: 50,
+     quantity: 3
+}
+
+const getAllProducts = () => {
+     axios.get(baseURL)
+     .then(productCallback)
+}
+
+
+
+
+
+
+const getButton = document.getElementById('getAll')
 console.log("ho")
 function sampleText() {
      const card = document.createElement('div')
-
-     //WHen a certain thing is sent from the home page
-     //Get request populate the from the database
-     //Select, delete, edit
-     //Complete by 2/20
-     card.innerHTML = `
-     <div> Words 
-     create box tag and append information here, add buttonstpp
-     YAYYYYYYYY</div>
-     `
+     card.style.display = 'flex';
+     const nameCell = document.createElement('div');
+     nameCell.textContent = database.name;
+     nameCell.style.flex = '1';
+     const proteinAmountCell = document.createElement('div');
+     proteinAmountCell.textContent = database.proteinAmount;
+     proteinAmountCell.style.flex = '1';
+     const priceCell = document.createElement('div');
+     priceCell.textContent = database.price;
+     priceCell.style.flex = '1';
+     const quantityCell = document.createElement('div');
+     quantityCell.textContent = database.quantity;
+     quantityCell.style.flex = '1'; 
+     card.appendChild(nameCell);
+     card.appendChild(proteinAmountCell);
+     card.appendChild(priceCell);
+     card.appendChild(quantityCell);
+     
+     mainBox.appendChild(card);
+     // card.innerHTML = `
+     // <div> ${database.name} </div>
+     // <div>${database.proteinAmount} </div>
+     // `
 
      mainBox.appendChild(card)
 }
 
-sampleText();
+function buttonTester() {
+
+}
+
+getButton.addEventListener('click', sampleText)
+
+// sampleText();
