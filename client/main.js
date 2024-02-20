9fdcf532afb7a1d9f49bdbbfc0d45bb2d7f3ffc9
Daniel Zeh <dfzeh1@gmail.com>
2024-02-19 20:58:51 -0600

    working

--- client/main.js
+++ client/main.js
@@ -6,6 +6,7 @@ function sampleText() {
      //WHen a certain thing is sent from the home page
      //Get request populate the from the database
      //Select, delete, edit
+     //Complete by 2/20
      card.innerHTML = `
      <div> Words 
      create box tag and append information here, add buttonstpp
