e27e519508d950ce7a983febbe78c58d0b71bc3c
Daniel Zeh <dfzeh1@gmail.com>
2024-02-26 11:23:42 -0600

    made populating button a function, changed opbject to list of objects

--- client/main.js
+++ client/main.js
@@ -1,10 +1,20 @@
 const mainBox = document.querySelector('.main')
-let database = {
-     name: "Protein",
-     proteinAmount: 100,
-     price: 50,
-     quantity: 3
-}
+let database = 
+     [
+          {name: "Protein",
+          proteinAmount: 100,
+          price: 50,
+          quantity: 3,
+     }
+     ,
+      {    name: "Protein",
+          proteinAmount: 100,
+          price: 50,
+          quantity: 3,
+
+      }
+     ]
+
 
 const getAllProducts = () => {
      axios.get('http:/localhost:4004/api/products')
@@ -14,28 +24,28 @@ const getAllProducts = () => {
 //Put Function
 //Counter????
 function getProductsScreen() {
-     const card = document.createElement('div')
-     card.style.display = 'flex';
-     //note to self, check on flex index soon 
-     const nameCell = document.createElement('div');
-     nameCell.textContent = database.name;
-     nameCell.style.flex = '1';
-     const proteinAmountCell = document.createElement('div');
-     proteinAmountCell.textContent = database.proteinAmount;
-     proteinAmountCell.style.flex = '1';
-     const priceCell = document.createElement('div');
-     priceCell.textContent = database.price;
-     priceCell.style.flex = '1';
-     const quantityCell = document.createElement('div');
-     quantityCell.textContent = database.quantity;
-     quantityCell.style.flex = '1'; 
-     card.appendChild(nameCell);
-     card.appendChild(proteinAmountCell);
-     card.appendChild(priceCell);
-     card.appendChild(quantityCell);
-     
-     mainBox.appendChild(card);
-     mainBox.appendChild(card)
+     for(let i = 0; i < database.length; i++){
+          const card = document.createElement('div')
+          card.style.display = 'flex';
+          const nameCell = document.createElement('div');
+          nameCell.textContent = database[i].name;
+          nameCell.style.flex = '1';
+          const proteinAmountCell = document.createElement('div');
+          proteinAmountCell.textContent = database[i].proteinAmount;
+          proteinAmountCell.style.flex = '1';
+          const priceCell = document.createElement('div');
+          priceCell.textContent = database[i].price;
+          priceCell.style.flex = '1';
+          const quantityCell = document.createElement('div');
+          quantityCell.textContent = database[i].quantity;
+          quantityCell.style.flex = '1'; 
+          card.appendChild(nameCell);
+          card.appendChild(proteinAmountCell);
+          card.appendChild(priceCell);
+          card.appendChild(quantityCell);
+          mainBox.appendChild(card);
+          mainBox.appendChild(card)
+     }
 }
 
 
@@ -74,6 +84,6 @@ function buttonTester() {
 
 }
 
-getButton.addEventListener('click', sampleText)
+getButton.addEventListener('click', getProductsScreen)
 
 // sampleText();
