82dd4d33514fe3af57d993a697c9de1e7831a21d
Daniel Zeh <dfzeh1@gmail.com>
2024-03-22 22:39:53 -0500

    nonsense

--- server/index.js
+++ server/index.js
@@ -12,13 +12,11 @@ const {
     createProduct
 } = require('../controller')
 
-
 app.get(`/api/products`, getProducts)
 // app.delete(`/api/products/:id`, deleteProduct)
 app.post(`/api/products`, createProduct)
 // app.put(`/api/product/:id`, updateProduct)
 
-console.log("hi")
-
+console.log("index.js running")
 const SERVER_PORT = '4004';
 app.listen(SERVER_PORT,() => console.log(`up and running on ${SERVER_PORT}`)) 
