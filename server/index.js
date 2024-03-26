b347183b3f921a482002af60b67f96374c033652
Daniel Zeh <dfzeh1@gmail.com>
2024-03-26 11:55:55 -0500

    index change

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
 app.use(cors());
 app.use(express.json());
-
 const {
     getProducts,
     // deleteProduct, 
@@ -16,7 +14,6 @@ app.get(`/api/products`, getProducts)
 // app.delete(`/api/products/:id`, deleteProduct)
 app.post(`/api/products`, createProduct)
 // app.put(`/api/product/:id`, updateProduct)
-
 console.log("index.js running")
 const SERVER_PORT = '4004';
 app.listen(SERVER_PORT,() => console.log(`up and running on ${SERVER_PORT}`)) 
